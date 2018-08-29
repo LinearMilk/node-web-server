f33f1e2bdbd3ad0f4f041c675a22df848094dfa0
LinearMilk <witold.henszel@gmail.com>
2018-08-29 13:06:07 +0200

    Setting up partials

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require("express");
 const hbs = require("hbs");
 
 app = express();
+hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  // res.send("<h1>Hello Express!</h1>");
-  res.send({
-    name: "Witold",
-    likes: ["games", "Luara", "drugs", "sex"]
+  res.render("home.hbs", {
+    pageTitle: "Home Page",
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: "Welcome to this uninteresting site."
   });
 });
 
