d4833f4b9596da838ef89ceef9185152f89fdd78
LinearMilk <witold.henszel@gmail.com>
2018-08-29 15:39:22 +0200

    configuring for heroku

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const hbs = require("hbs");
-
+const fs = require("fs");
+const port = process.env.PORT || 3000;
 app = express();
+
 hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) {
+      console.log("Unable to append to server log");
+    }
+  });
+  console.log(log);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render("maintenance.hbs");
+});
+
 app.use(express.static(__dirname + "/public"));
 
 hbs.registerHelper("getCurrentYear", () => {
@@ -31,6 +50,6 @@ app.get("/bad", (req, res) => {
   res.send({ errorMessage: "Error handling this request." });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
