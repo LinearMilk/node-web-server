a9b9ec5980a8be6216b9cef51cc8a961ee3fa876
LinearMilk <witold.henszel@gmail.com>
2018-08-29 12:29:24 +0200

    Adding handlebars

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const hbs = require("hbs");
 
 app = express();
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.send("Hello Express!");
+  // res.send("<h1>Hello Express!</h1>");
+  res.send({
+    name: "Witold",
+    likes: ["games", "Luara", "drugs", "sex"]
+  });
 });
 
-app.listen(3000);
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+app.get("/bad", (req, res) => {
+  res.send({ errorMessage: "Error handling this request." });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000");
+});
